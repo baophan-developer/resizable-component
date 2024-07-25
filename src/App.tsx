0851ad728d9917a7f56b40c0b1fa0ae8b58a2fd9
BaoPhan <baophan.developer@gmail.com>
2024-07-25 17:38:12 +0700

    add component ResizableComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
+import {PropsWithChildren, useEffect, useRef} from "react";
 import "./App.css";
 
+type ResizableComponentProps = {
+	width?: string | number;
+	height?: string | number;
+};
+const ResizableComponent = (props: PropsWithChildren<ResizableComponentProps>) => {
+	const {children} = props;
+
+	const resizerRef = useRef<HTMLDivElement>(null);
+	const mainRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		return () => {};
+	}, []);
+
+	return (
+		<div>
+			<div ref={mainRef} style={{}}>
+				{children}
+			</div>
+			<div ref={resizerRef} style={{}} />
+		</div>
+	);
+};
+
 function App() {
-	return <></>;
+	return (
+		<div
+			style={{
+				width: "100vw",
+				height: "100vh",
+				display: "flex",
+				alignItems: "center",
+				justifyContent: "center",
+			}}
+		>
+			<ResizableComponent>Resizable component...</ResizableComponent>
+		</div>
+	);
 }
 
 export default App;
